860e84cca6788569919196dc48e8afae840f2df4
Josh Harvey <josh@lamassu.is>
2014-08-22 12:59:33 -0400

    fix(main) various bug fixes

--- test/arq.js
+++ test/arq.js
@@ -1,21 +1,93 @@
 'use strict';
 
-require('chai').should();
+var should = require('chai').should();
 var Device = require('./mocks/device');
-var serverDevice = new Device();
-var clientDevice = new Device(serverDevice);
-
 var ARQ = require('../lib/arq');
 
-var serverArq = new ARQ(serverDevice);
-var clientArq = new ARQ(clientDevice);
+function createARQs(opts) {
+  opts = opts || {};
+  var serverDevice = new Device();
+  var clientDevice = new Device(serverDevice, opts);
+  var serverArq = new ARQ(serverDevice);
+  var clientArq = new ARQ(clientDevice);
+
+  clientArq.on('error', function() {});
+  serverArq.on('error', function() {});
+
+  return {client: clientArq, server: serverArq};
+}
 
 describe('ARQ', function() {
+
   it('should send hello world', function(done) {
-    serverArq.on('message', function(message) {
+    var arqs = createARQs();
+    arqs.server.on('message', function(message) {
       message.should.equal('Hello World!');
       done();
     });
-    clientArq.send('Hello World!', function() {});
+    arqs.client.send('Hello World!', function() {});
+  });
+
+  it('should send hello world on noisy channel', function(done) {
+    var arqs = createARQs({noisy: true});
+    arqs.server.on('message', function(message) {
+      message.should.equal('Hello World!');
+      done();
+    });
+    arqs.client.send('Hello World!', function() {});
+  });
+
+  it('should fail sending two messages, fast', function(done) {
+    var arqs = createARQs();
+
+    arqs.server.on('message', function() {});
+    arqs.client.send('Hello World!', function() {});
+    setTimeout(function() {
+      arqs.client.send('World Hello!', function (err) {
+        should.exist(err);
+        err.message.should.equal('Previous send in progress');
+        done();
+      });
+    }, 50);
+  });
+
+  it('should send two messages, slowly', function(done) {
+    var arqs = createARQs();
+
+    var messages = [];
+    arqs.server.on('message', function(message) {
+      messages.push(message);
+      if (messages.length === 2) {
+        messages[0].should.equal('Hello World!');
+        messages[1].should.equal('World Hello!');
+        done();
+      }
+    });
+    arqs.client.send('Hello World!', function() {});
+    setTimeout(function() {
+      arqs.client.send('World Hello!', function() {});
+    }, 300);
+  });
+
+  it('should send three messages, slowly', function(done) {
+    var arqs = createARQs();
+
+    var messages = [];
+    arqs.server.on('message', function(message) {
+      messages.push(message);
+      if (messages.length === 3) {
+        messages[0].should.equal('Hello World!');
+        messages[1].should.equal('World Hello!');
+        messages[2].should.equal('World Hello 3!');
+        done();
+      }
+    });
+    arqs.client.send('Hello World!', function() {});
+    setTimeout(function() {
+      arqs.client.send('World Hello!', function() {});
+      setTimeout(function() {
+        arqs.client.send('World Hello 3!', function() {});
+      }, 300);
+    }, 300);
   });
 });
--- test/mocks/device.js
+++ test/mocks/device.js
@@ -3,19 +3,31 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-var Device = function(server) {
+var ACK = 0x06;
+
+var Device = function(server, opts) {
   EventEmitter.call(this);
   if (server) {
     this.peer = server;
     server.peer = this;
   }
+  opts = opts || {};
+  this.noisy = opts.noisy;
+  this.count = 0;
 };
 
 util.inherits(Device, EventEmitter);
 module.exports = Device;
 
-Device.prototype.write = function write(data) {
+Device.prototype.write = function write(originalData) {
   var self = this;
+
+  var data = new Buffer(originalData);
+
+  if (this.count === 0 && this.noisy && data[0] !== ACK) data[6] = data[6] ^ 0x4;
+  if (this.count === 1 && this.noisy && data[0] !== ACK) data[6] = data[6] ^ 0x3;
+
+  if (data[0] !== ACK) this.count++;
   setTimeout(function () {
     self.peer.incomingMessage(new Buffer(data));
   }, 50);
